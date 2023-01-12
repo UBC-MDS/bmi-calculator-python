e1b9c98f5f64c3d8cca131894e3ee5df276e8c5b
hcwang24 <hcwang24@student.ubc.ca>
2023-01-12 10:37:18 -0800

    feat(calculate_bmi): add in a placeholder

---
+++ src/bmi_calculator/calculate_bmi.py
@@ -0,0 +1,31 @@
+def calculate_bmi(weight, height, return_graph=False):
+    """Calculate the body mass index (BMI) based on current weight and height.
+
+    Body Mass Index (BMI) is a calculated measure of weight relative to height,
+    defined as body weight in kilograms divided by height in meters squared
+    (Keys et al., 1972). If `return_graph` is `True`, an `alt.Chart` object
+    instead of a bmi value.
+
+    Parameters
+    ----------
+    weight : float
+        Weight, in kilograms.
+    height : float
+        Height, in meters.
+    return_graph : bool
+        Whether to return a graph instead of a value
+    
+    Returns
+    -------
+    float or alt.Chart
+        If `return_graph` is `False`, we get a value of the body mass index. 
+        If `return_graph` is True, we get an `alt.Chart` instead that can be
+        saved or shared. Could be a simplified version of 
+        https://cdn.dribbble.com/users/31916/screenshots/310892/bmi-chart.png
+    """
+    if return_graph == False:
+        return (weight/(height ** 2))
+    else:
+        # return graph here.
+        pass
+    
